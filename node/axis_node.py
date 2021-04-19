a7ab55359e87fbd4eb5295a8a84c52bffe76efb5
QuartzYan <thesuperstring@outlook.com>
2021-04-19 18:08:15 +0800

    fixed some problems

--- node/axis_node.py
+++ node/axis_node.py
@@ -11,21 +11,21 @@ from axis import axis
 
 
 def main():
-    rospy.init_node("axis_camera")
-    hostname = rospy.get_param("hostname", default="192.168.80.1")
-    username = rospy.get_param("username", default="iqr")
-    password = rospy.get_param("password", default="123")
-    frame_id = rospy.get_param("frame_id", default="axis_image")
-    p5635 = axis(hostname, username, password, frame_id)
+  rospy.init_node("axis_camera")
+  hostname = rospy.get_param("hostname", default="192.168.80.1")
+  username = rospy.get_param("username", default="iqr")
+  password = rospy.get_param("password", default="123")
+  frame_id = rospy.get_param("frame_id", default="axis_image")
+  p5635 = axis(hostname, username, password, frame_id)
 
-    while True:
-        try:
-            if p5635.getImage():
-                p5635.pubImage()
-                p5635.pubImageInfo()
-        except:
-            rospy.logerr("Timed out while trying to get message.")
+  while not rospy.is_shutdown():
+    try:
+      if p5635.getImage():
+        p5635.pubImage()
+        #p5635.pubImageInfo()
+    except:
+      rospy.logerr("Timed out while trying to get message.")
 
 
 if __name__ == "__main__":
-    main()
+  main()
--- node/axis_ptz_node.py
+++ node/axis_ptz_node.py
@@ -11,26 +11,24 @@ from axis_ptz import axis_ptz
 
 
 def main():
-    rospy.init_node("axis_ptz")
-    hostname = rospy.get_param("hostname", default="192.168.80.1")
-    username = rospy.get_param("username", default="iqr")
-    password = rospy.get_param("password", default="123")
-    enable_joint = rospy.get_param("enable_joint", default=True)
-    ptz_pub_rate = rospy.get_param("ptz_pub_rate", default=10)
-    pan_joint = rospy.get_param("pan_joint", default="pan_joint")
-    tilt_joint = rospy.get_param("tilt_joint", default="tilt_joint")
+  rospy.init_node("axis_ptz")
+  hostname = rospy.get_param("hostname", default="192.168.80.1")
+  username = rospy.get_param("username", default="iqr")
+  password = rospy.get_param("password", default="123")
+  enable_joint = rospy.get_param("enable_joint", default=True)
+  ptz_pub_rate = rospy.get_param("ptz_pub_rate", default=10)
+  pan_joint = rospy.get_param("pan_joint", default="pan_joint")
+  tilt_joint = rospy.get_param("tilt_joint", default="tilt_joint")
 
-    
-    def loop():
-        r = rospy.Rate(ptz_pub_rate)
-        p5635_ptz = axis_ptz(hostname, username, password, enable_joint, pan_joint, tilt_joint)
-        while True:
-            p5635_ptz.pub_ptz_msg()
-            r.sleep()
-    t = threading.Thread(target=loop)
-    t.start()
-    rospy.spin()
-    
+  def loop():
+    r = rospy.Rate(ptz_pub_rate)
+    p5635_ptz = axis_ptz(hostname, username, password, enable_joint, pan_joint, tilt_joint)
+    while not rospy.is_shutdown():
+      p5635_ptz.pub_ptz_msg()
+      r.sleep()
+  t = threading.Thread(target=loop)
+  t.start()
+  rospy.spin()
 
 if __name__ == "__main__":
-    main()
+  main()
